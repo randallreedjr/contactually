064732bd902539f171cedd38e7aa045991b192d2
Randall Reed, Jr <randallreedjr@users.noreply.github.com>
2017-09-01 17:27:05 -0400

    Delete via ajax

--- app/assets/javascripts/components/contact.jsx
+++ app/assets/javascripts/components/contact.jsx
@@ -1,28 +1,33 @@
 
 
 class Contact extends React.Component {
+  handleDelete(){
+    this.props.delete(this.props.id)
+  }
+
   render() {
     return (
       <tr>
-        <td><Name firstName={this.props.firstName} lastName={this.props.lastName} url={this.props.showUrl}/></td>
+        <td><Name firstName={this.props.firstName} lastName={this.props.lastName} url={`/contacts/${this.props.id}`}/></td>
         <td><Email emailAddress={this.props.emailAddress} /></td>
         <td><Phone phoneNumber={this.props.phoneNumber} /></td>
         <td><Extension extension={this.props.extension} /></td>
         <td className='company-name'>{this.props.companyName}</td>
         <td><a href={this.props.editUrl}>Edit</a></td>
-        <td><a href='#'>Delete</a></td>
+        <td><button onClick={this.handleDelete.bind(this)}>Delete</button></td>
       </tr>
     )
   }
 }
 
 Contact.propTypes = {
+  id: React.PropTypes.number.isRequired,
   firstName: React.PropTypes.string.isRequired,
   lastName: React.PropTypes.string,
   emailAddress: React.PropTypes.string,
   phoneNumber: React.PropTypes.string,
   extension: React.PropTypes.string,
   companyName: React.PropTypes.string,
-  showUrl: React.PropTypes.string,
-  editUrl: React.PropTypes.string
+  editUrl: React.PropTypes.string,
+  delete: React.PropTypes.func.isRequired
 }
