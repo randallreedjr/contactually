389db8f8caab54082df062e57faef2c419deba59
Randall Reed, Jr <randallreedjr@users.noreply.github.com>
2017-08-31 21:28:46 -0400

    Rename fields to match upload

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170831213907) do
+ActiveRecord::Schema.define(version: 20170901002607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,12 +25,12 @@ ActiveRecord::Schema.define(version: 20170831213907) do
   create_table "contacts", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email"
-    t.string   "phone"
+    t.string   "email_address"
+    t.string   "phone_number"
     t.string   "extension"
     t.integer  "company_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   add_index "contacts", ["company_id"], name: "index_contacts_on_company_id", using: :btree
